79d7da650b51fa84fdc6896a5d6ff2168da7875d
bertanilhan <bertanilhan@DESKTOP-CBQLSO0>
2017-07-08 01:10:02 +0300

    "IVariableModule" ve "IConstantModule" için reflection örneği eklendi.

--- PrintManagerSwitchDiNetExample.WebUi/Global.asax.cs
+++ PrintManagerSwitchDiNetExample.WebUi/Global.asax.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 using Ninject.Modules;
+using PrintManagerSwitchDiNetExample.Business.Abstract;
 using PrintManagerSwitchDiNetExample.Business.DependencyResolvers;
 using PrintManagerSwitchDiNetExample.WebUi.Utilities;
 
@@ -17,6 +19,7 @@ namespace PrintManagerSwitchDiNetExample.WebUi
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
 
             //Sabit olan modüller bu alana dizilecektir.
             var constantModule = new INinjectModule[]
@@ -32,6 +35,14 @@ namespace PrintManagerSwitchDiNetExample.WebUi
                 new XeroxBusinessModule(),
                 new BaseBusinessModule()
             };
+
+            //Eğer modüllerin otomatik olarak gelmesini istersek modülleri "IVariableModule" ve "IConstantModule" ile işaretlemek gerekir
+            //Reflection kodu NinjectInstanceResolverFactory yazılır.
+
+            //var moduleTypes = AppDomain.CurrentDomain.GetAssemblies()
+            //    .SelectMany(s => s.GetTypes())
+            //    .Where(p => typeof(IVeriableModule).IsAssignableFrom(p) && !p.IsInterface);
+
             ControllerBuilder.Current.SetControllerFactory(new NinjectControllerFactory(new NinjectInstanceResolverFactory(veriableModules,constantModule)));
         }
     }
